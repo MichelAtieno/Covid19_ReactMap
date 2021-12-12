bd403612e7ad2fb338bdd94e99fc64b82fd5d1f8
Michel Atieno <mishqamish@gmail.com>
2021-12-12 16:06:42 +0300

    Country colors correspond to number of cases

--- src/tasks/loadCountriesTask.js
+++ src/tasks/loadCountriesTask.js
@@ -1,5 +1,6 @@
 import papa from "papaparse";
 import {features} from '../data/countries.json';
+import legendItems from '../entities/legendItems';
 
 class LoadCountriesTask {
     covid19DataUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv";
@@ -33,15 +34,25 @@ class LoadCountriesTask {
                 mapCountry.properties.confirmed = confirmed;
                 mapCountry.properties.confirmedText = this.#formatNumberWithCommas(confirmed);
             }
-        
+            
+            this.#setCountryColor(mapCountry);
         }
 
         this.setState(this.mapCountries);
     };
 
+    #setCountryColor = (mapCountry) => {
+        const legendItem = legendItems.find((legendItem) =>
+            legendItem.isFor(mapCountry.properties.confirmed)
+     );
+     if(legendItem != null) {
+         mapCountry.properties.color = legendItem.color;
+     }
+    };
+
     #formatNumberWithCommas = (number) => {
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+    };
 }
 
 
